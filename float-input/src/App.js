dfb19f6c721df24db9cc1e8565e24d9556f3fd34
jellyfisharuka <arukeulen@gmail.com>
2023-12-18 23:31:40 +0600

    fixing the errors

--- float-input/src/App.js
+++ float-input/src/App.js
@@ -19,36 +19,35 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
       getData();
     }, []);
     return (
-      <Routes>
-      <Route path="/" element={<Layout />}>
-        {/* public routes */}
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
+      <><Routes>
+        <Route path="/" element={<Layout />}>
+          {/* public routes */}
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
         </Route>
-        </Routes>
-      <div className="box-form">
-        <div className="left">
-        
-          <div className="overlay"></div>
-        </div>
-        <div className="right" style={{ height: '100%' }}>
-          <h5></h5>
-          <p><a href="#"></a></p>
-          <div className="inputs1">
-            <input type="text" /><label htmlFor="text">Username</label>
+      </Routes><div className="box-form">
+          <div className="left">
+
+            <div className="overlay"></div>
+          </div>
+          <div className="right" style={{ height: '100%' }}>
+            <h5></h5>
+            <p><a href="#"></a></p>
+            <div className="inputs1">
+              <input type="text" /><label htmlFor="text">Username</label>
+              <br />
+            </div>
+            <div className="inputs2">
+              <input type="password" /><label htmlFor="password">Password</label>
+              <br />
+            </div>
+            <div className="remember-me--forget-password">
+              <p>Forgot password?</p>
+            </div>
             <br />
+            <button>Login</button>
           </div>
-          <div className="inputs2">
-            <input type="password" /><label htmlFor="password">Password</label>
-            <br />
-          </div>
-          <div className="remember-me--forget-password">
-            <p>Forgot password?</p>
-          </div>
-          <br />
-          <button>Login</button>
-        </div>
-      </div>
+        </div></>
     );
   }
   
