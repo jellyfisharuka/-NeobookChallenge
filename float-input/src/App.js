44bc1ac83e42a6689dc1b91ba8da528b52220612
jellyfisharuka <arukeulen@gmail.com>
2023-12-19 23:44:32 +0600

    formik, react-toastify modules

--- float-input/src/App.js
+++ float-input/src/App.js
@@ -2,12 +2,33 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useFormik } from 'formik'; // Импорт useFormik
 
 import Register from './components/register_auth';
-import Layout from './components/Layout'; // Предполагается, что у вас есть компонент Layout
-import Login from './components/Login'; // Предполагается, что у вас есть компонент Login
+import Layout from './components/Layout';
+import Login from './components/Login';
 
 function App() {
+  // Используем useFormik для управления формой
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: async (values) => {
+      try {
+        // Отправляем данные на сервер, используя axios или другую библиотеку для HTTP-запросов
+        const response = await axios.post('https://neobook.online/mobi-market/swagger/', values);
+        console.log('Ответ сервера:', response.data);
+
+        // Здесь можно добавить логику успешного входа
+      } catch (error) {
+        console.error('Ошибка при отправке данных:', error);
+        toast.error('Ошибка входа. Попробуйте еще раз.'); // Показываем сообщение об ошибке с помощью react-toastify
+      }
+    },
+  });
+
   useEffect(() => {
     const getData = async () => {
       try {
@@ -26,36 +47,48 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-          {/* public routes */}
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
       </Routes>
-      <div className="box-form">
-        <div className="left">
-          <div className="overlay"></div>
-        </div>
-        <div className="right" style={{ height: '100%' }}>
-          <h5></h5>
-          <p><a href="#"></a></p>
-          <div className="inputs1">
-            <input type="text" /><label htmlFor="text">Username</label>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="box-form">
+          <div className="left">
+            <div className="overlay"></div>
+          </div>
+          <div className="right" style={{ height: '100%' }}>
+            <h5></h5>
+            <p><a href="#"></a></p>
+            <div className="inputs1">
+              <input
+                type="text"
+                name="username"
+                value={formik.values.username}
+                onChange={formik.handleChange}
+              />
+              <label htmlFor="text">Username</label>
+              <br />
+            </div>
+            <div className="inputs2">
+              <input
+                type="password"
+                name="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+              />
+              <label htmlFor="password">Password</label>
+              <br />
+            </div>
+            <div className="remember-me--forget-password">
+              <p>Forgot password?</p>
+            </div>
             <br />
+            <button type="submit">Login</button>
           </div>
-          <div className="inputs2">
-            <input type="password" /><label htmlFor="password">Password</label>
-            <br />
-          </div>
-          <div className="remember-me--forget-password">
-            <p>Forgot password?</p>
-          </div>
-          <br />
-          <button onClick={handleLogin}>Login</button>
         </div>
-      </div>
+      </form>
     </Router>
   );
 }
 
 export default App;
-
