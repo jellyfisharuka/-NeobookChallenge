6649c897359293b37ea2852f19d19c714029ca7c
jellyfisharuka <arukeulen@gmail.com>
2023-12-20 23:42:56 +0600

    fixed my previous error

--- float-input/src/App.js
+++ float-input/src/App.js
@@ -1,49 +1,63 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useFormik } from 'formik'; // Импорт useFormik
-
-import Register from './components/register_auth';
-import Layout from './components/Layout';
-import Login from './components/Login';
+import { useFormik } from 'formik';
 
 function App() {
-  useEffect(() => {
-    const getData = async () => {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: async (values) => {
       try {
-        const response = await axios.get(
-          'https://neobook.online/mobi-market/swagger/'
-        );
-        console.log(response.data);
-      } catch (e) {
-        console.log(e);
+        const response = await axios.post('https://neobook.online/mobi-market/swagger/', values);
+        console.log('Ответ сервера:', response.data);
+
+        // Здесь можно добавить логику успешного входа
+      } catch (error) {
+        console.error('Ошибка при отправке данных:', error);
+        toast.error('Ошибка входа. Попробуйте еще раз.');
       }
-    };
-    getData();
-  }, []);
+    },
+  });
+
   return (
     <div className="box-form">
-      <div className="left">
-        <div className="overlay"></div>
-      </div>
-      <div className="right" style={{ height: '100%' }}>
-        <h5></h5>
-        <p><a href="#"></a></p>
-        <div className="inputs1">
-          <input type="text" /><label htmlFor="text">Username</label>
+      <form onSubmit={formik.handleSubmit}> {/* Добавляем форму */}
+        <div className="left">
+          <div className="overlay"></div>
+        </div>
+        <div className="right" style={{ height: '100%' }}>
+          <h5></h5>
+          <p><a href="#"></a></p>
+          <div className="inputs1">
+            <input
+              type="text"
+              name="username"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+            />
+            <label htmlFor="text">Username</label>
+            <br />
+          </div>
+          <div className="inputs2">
+            <input
+              type="password"
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+            />
+            <label htmlFor="password">Password</label>
+            <br />
+          </div>
+          <div className="remember-me--forget-password">
+            <p>Forgot password?</p>
+          </div>
           <br />
+          <button type="submit">Login</button> {/* Добавляем кнопку отправки формы */}
         </div>
-        <div className="inputs2">
-          <input type="password" /><label htmlFor="password">Password</label>
-          <br />
-        </div>
-        <div className="remember-me--forget-password">
-          <p>Forgot password?</p>
-        </div>
-        <br />
-        <button>Login</button>
-      </div>
+      </form>
     </div>
   );
 }
